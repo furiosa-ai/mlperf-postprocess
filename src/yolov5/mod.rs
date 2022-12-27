6ede5e2dbf225489e9c1df274f4b17104d6584a5
MyeongGeun Shin <mg.shin@furiosa.ai>
2022-12-27 14:06:15 +0900

    fix: separate rust logic / python bindings

--- src/yolov5/mod.rs
+++ src/yolov5/mod.rs
@@ -1,61 +1,19 @@
 use std::fmt;
 
 use itertools::izip;
-use numpy::ndarray::{Array2, Array3, Dim};
-use numpy::{PyArray, PyReadonlyArray3, PyReadonlyArray5};
+use numpy::ndarray::{Array2, Array3};
+use numpy::{PyArray, PyArray2, PyReadonlyArray3, PyReadonlyArray5};
 use pyo3::prelude::*;
 
 use crate::common::ssd_postprocess::{BoundingBox, CenteredBox};
 
-/// YOLOv5 PostProcessor
+/// Returns max value's index and value (Argmax, Max)
 ///
-/// It takes anchors, class_names, strides as input
-///
-/// Args:
-///     anchors (numpy.ndarray)
-///     class_names (List[str])
-///     strides (numpy.ndarray)
-#[pyclass]
-#[pyo3(
-    text_signature = "(anchors: numpy.ndarray, class_names: Sequence[str], strides: numpy.ndarray)"
-)]
-#[derive(Debug, Clone)]
-pub struct RustPostProcessor {
-    #[pyo3(get)]
-    pub num_classes: usize,
-    #[pyo3(get)]
-    pub num_outputs: usize,
-    #[pyo3(get)]
-    pub num_detection_layers: usize,
-    #[pyo3(get)]
-    pub num_anchor: usize,
-    pub anchors: Array3<f32>,
-    #[pyo3(get)]
-    pub class_names: Vec<String>,
-    #[pyo3(get)]
-    pub strides: Vec<f32>,
-}
-
-impl fmt::Display for RustPostProcessor {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "RustPostProcessor {{ num_classes: {}, num_outputs: {}, \
-            num_detection_layers: {}, num_anchor: {}, strides: {:?} }}",
-            self.num_classes,
-            self.num_outputs,
-            self.num_detection_layers,
-            self.num_anchor,
-            self.strides
-        )
-    }
-}
-
+/// Copied from https://docs.rs/rulinalg/latest/src/rulinalg/utils.rs.html#245-261
 pub fn argmax<T>(u: &[T]) -> (usize, T)
 where
     T: Copy + PartialOrd,
 {
-    // Copied from https://docs.rs/rulinalg/latest/src/rulinalg/utils.rs.html#245-261
     // Length is always nonzero
     // assert!(u.len() != 0);
 
@@ -72,50 +30,49 @@ where
     (max_index, max)
 }
 
-#[pymethods]
-impl RustPostProcessor {
-    #[new]
-    fn new(
-        anchors: PyReadonlyArray3<'_, f32>,
-        class_names: Vec<String>,
-        strides: Vec<f32>,
-    ) -> Self {
-        let anchors = anchors.to_owned_array();
+#[derive(Debug, Clone)]
+pub struct RustPostprocessor {
+    pub num_classes: usize,
+    pub num_outputs: usize,
+    pub num_detection_layers: usize,
+    pub num_anchor: usize,
+    pub anchors: Array3<f32>,
+    pub strides: Vec<f32>,
+}
+
+impl fmt::Display for RustPostprocessor {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "RustPostProcessor {{ num_classes: {}, num_outputs: {}, \
+            num_detection_layers: {}, num_anchor: {}, strides: {:?} }}",
+            self.num_classes,
+            self.num_outputs,
+            self.num_detection_layers,
+            self.num_anchor,
+            self.strides
+        )
+    }
+}
+
+impl RustPostprocessor {
+    fn new(anchors: Array3<f32>, num_classes: usize, strides: Vec<f32>) -> Self {
         let shape = anchors.shape();
         Self {
-            num_classes: class_names.len(),
-            num_outputs: class_names.len() + 5,
+            num_classes,
+            num_outputs: num_classes + 5,
             num_detection_layers: shape[0],
             num_anchor: shape[1],
             anchors,
-            class_names,
             strides,
         }
     }
 
-    fn __repr__(&self) -> PyResult<String> {
-        Ok(format!("{:?}", self))
-    }
-
-    fn __str__(&self) -> PyResult<String> {
-        Ok(format!("{}", self))
-    }
-
-    /// Evaluate the postprocess
-    ///
-    /// Args:
-    ///     inputs (Sequence[numpy.ndarray]): Input tensors
-    ///     conf_threshold (float): confidence threshold
-    ///
-    /// Returns:
-    ///     numpy.ndarray: Output tensors
-    /// #[pyo3(text_signature = "(self, inputs: Sequence[numpy.ndarray], conf_threshold: float)")]
-    fn eval(
+    fn box_decode(
         &self,
-        py: Python<'_>,
         inputs: Vec<PyReadonlyArray5<'_, f32>>,
         conf_threshold: f32,
-    ) -> PyResult<Py<PyArray<f32, Dim<[usize; 2]>>>> {
+    ) -> Array2<f32> {
         const MAX_BOXES: usize = 10_000;
         const NUM_COLS: usize = 6;
         let mut results = Vec::new();
@@ -180,8 +137,59 @@ impl RustPostProcessor {
                 }
             }
         }
-        Ok(PyArray::from_array(py, &Array2::from_shape_vec((num_rows, NUM_COLS), results).unwrap())
-            .to_owned())
+        Array2::from_shape_vec((num_rows, NUM_COLS), results).unwrap()
+    }
+}
+
+/// YOLOv5 PostProcessor
+///
+/// It takes anchors, class_names, strides as input
+///
+/// Args:
+///     anchors (numpy.ndarray)
+///     num_classes (int)
+///     strides (numpy.ndarray)
+#[pyclass]
+#[pyo3(
+    text_signature = "(anchors: numpy.ndarray, class_names: Sequence[str], strides: numpy.ndarray)"
+)]
+pub struct RustPostProcessor(RustPostprocessor);
+
+#[pymethods]
+impl RustPostProcessor {
+    #[new]
+    fn new(
+        anchors: PyReadonlyArray3<'_, f32>,
+        num_classes: usize,
+        strides: Vec<f32>,
+    ) -> PyResult<Self> {
+        Ok(Self(RustPostprocessor::new(anchors.to_owned_array(), num_classes, strides)))
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("{:?}", self.0))
+    }
+
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("{}", self.0))
+    }
+
+    /// Evaluate the postprocess
+    ///
+    /// Args:
+    ///     inputs (Sequence[numpy.ndarray]): Input tensors
+    ///     conf_threshold (float): confidence threshold
+    ///
+    /// Returns:
+    ///     numpy.ndarray: Output tensors
+    /// #[pyo3(text_signature = "(self, inputs: Sequence[numpy.ndarray], conf_threshold: float)")]
+    fn eval(
+        &self,
+        py: Python<'_>,
+        inputs: Vec<PyReadonlyArray5<'_, f32>>,
+        conf_threshold: f32,
+    ) -> PyResult<Py<PyArray2<f32>>> {
+        Ok(PyArray::from_array(py, &self.0.box_decode(inputs, conf_threshold)).to_owned())
     }
 }
 
